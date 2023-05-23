8e613f3add4f2ee7d1248034f1f86167ab4afe56
agata <agata.pysz@gmail.com>
2023-05-23 08:15:45 +0200

    adding endpoint for retrival all questions traces

--- src/main/java/org/pysz/questy/persistnce/QuestionTrace.java
+++ src/main/java/org/pysz/questy/persistnce/QuestionTrace.java
@@ -1,6 +1,9 @@
 package org.pysz.questy.persistnce;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,19 +12,22 @@ import java.time.LocalDateTime;
 @Entity
 public class QuestionTrace {
 
-  public QuestionTrace(String questionId, int viewCount, LocalDateTime timestamp) {
-    this.questionId = questionId;
-    this.viewCount = viewCount;
-    this.timestamp = timestamp;
-  }
+    public QuestionTrace(String questionId, int viewCount, LocalDateTime timestamp) {
+        this.questionId = questionId;
+        this.viewCount = viewCount;
+        this.timestamp = timestamp;
+    }
 
-  @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
-  private Integer id;
+    public QuestionTrace() {
+    }
 
-  private String questionId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
-  private int viewCount;
+    private String questionId;
 
-  private LocalDateTime timestamp;
+    private int viewCount;
+
+    private LocalDateTime timestamp;
 }
